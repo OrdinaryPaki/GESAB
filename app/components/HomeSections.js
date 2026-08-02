e48921176fcfe8c626ca57a3e2120f77603b5ab3
Adam Chaudhry <adam.chaud05@gmail.com>
2026-08-02 23:05:59 +0200

    Remove process section from homepage

--- app/components/HomeSections.js
+++ app/components/HomeSections.js
@@ -23,7 +23,6 @@ export function HomePage() {
       <AboutPreview />
       <TeamSection />
       <Testimonials />
-      <ProcessSection />
       <GallerySection />
       <FaqSection />
       <ContactBand />
