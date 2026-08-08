1082bc66ba0670bbd1d48381da7b602e5dc194a8
Adam Chaudhry <adam.chaud05@gmail.com>
2026-08-08 14:31:01 +0200

    feat: improve home mobile spacing

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { HomePageFidelity } from "./components/HomePageFidelity";
 import "./home-fidelity.css";
+import "./responsive/home-mobile.css";
 
 export default function Home() {
   return <HomePageFidelity />;
